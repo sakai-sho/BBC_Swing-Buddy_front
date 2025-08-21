d19ce42f4434f24db22fd70a6ba5ea603d95fab9
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-08-21 22:36:33 +0900

    Fix .gitignore and GitHub Actions configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    typedRoutes: true
-  }
+  output: 'standalone'
 }
 
 export default nextConfig
